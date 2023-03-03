0607816e9bcfbb360d92f8cdcb3f077e41057ea7
Ralph <ramponsah026@st.ug.edu.gh>
2023-03-03 14:33:15 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-
+import { astroImageTools } from "astro-imagetools";
 // https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), astroImageTools],
+  output: "server",
+  adapter: vercel()
 });
