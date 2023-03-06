e83f244de3c7407760f3ab3cee83fe89a8d98bfc
Ralph <ramponsah026@st.ug.edu.gh>
2023-03-06 13:01:37 +0000

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,16 @@
 import { defineConfig } from 'astro/config';
-import { astroImageTools } from "astro-imagetools";
-// https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
-
-// https://astro.build/config
-import compress from "astro-compress";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), astroImageTools, react()],
-  output: "server",
-  adapter: netlify()
+  integrations: [image(), react()]
 });
